5e7cbdefd7449a863751d2ca24875bc6accfa7dc
sahilbheke <sahil.bheke@joshsoftware.com>
2025-01-16 02:00:50 +0530

    Add whats_the_day.go, Which contains the Day3 problem1 solution. Add word_count.go, Which contains the Day3 problem2 solution. Add return_the_slices.go, Which contains the Day3 problem3 solution. (with better formatting)

--- Day3/whats_the_day.go
+++ Day3/whats_the_day.go
@@ -2,31 +2,35 @@ package main
 
 import "fmt"
 
-func main() {
-//	Write a program to store the day(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) against the respective index of the day(1, 2, 3, 4, 5, 6, 7) in a map.
-//You can hard-code the map in your program.
-//Take an integer input from the user and print the day stored against that index and if nothing is to be found for that index,
-//Print "Not a day"
-//Hint: Following code can be used to take an integer input from the user in the GO Programming Language
-//var index int
-//fmt.Scanln(&index)
-//Example Test Case:
-//Input: 5
-//Output: Friday
+/* 2. Write a program to store the day(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday) against the respective index of the day(1, 2, 3, 4, 5, 6, 7) in a map.
+You can hard-code the map in your program.
+Take an integer input from the user and print the day stored against that index and if nothing is to be found for that index,
+Print "Not a day"
+Hint: Following code can be used to take an integer input from the user in the GO Programming Language
+var index int
+fmt.Scanln(&index)
+Example Test Case:
+Input: 5
+Output: Friday
 
-//Input: 11
-//Output: Not a day		
-	
-var maap = map[int]string {1:"Monday", 2:"Tuesday", 3:"Wednesday", 4:"Thrusday", 5:"Friday", 6:"Saturday", 7:"Sunday"}
-	
-	var num int
+Input: 11
+Output: Not a day
+*/
+
+var maap = map[int]string{1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thrusday", 5: "Friday", 6: "Saturday", 7: "Sunday"}
+
+func main() {
+
+	var index int
 	fmt.Print("Enter index between 1 to 8 to get respective day: ")
-	fmt.Scan(&num)
-	
-	value, exist := maap[num]
-	if exist{
+	fmt.Scan(&index)
+
+	// Getting respective day stored against given index
+	value, exist := maap[index]
+
+	if exist {
 		fmt.Println("The day against the respective index is:", value)
-	}else{
+	} else {
 		fmt.Println("Not a day!")
 	}
 }
