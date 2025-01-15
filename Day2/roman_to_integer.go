4a1ad83f8ecb7b323567c0fd0f3fa21c73d395fe
sahilbheke <sahil.bheke@joshsoftware.com>
2025-01-15 13:11:20 +0530

    Add Optimized code for roman to integer code. Trim comments. Make it modular.

--- Day2/roman_to_integer.go
+++ Day2/roman_to_integer.go
@@ -41,7 +41,7 @@ func main() {
 
 		validRomanPattern := `^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$`
 
-		//Regex Compilation
+		// Regex Compilation
 		pattern := regexp.MustCompile(validRomanPattern)
 
 		//Validation
